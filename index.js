b7a406afd35d82e19c5ea9da8740fd9eb789d62a
Dwight Carter <dwightcarter@gpinet.com>
2022-03-24 08:51:09 -0400

    first push

---
+++ index.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let titleElement = document.getElementsByTagName('title')[0];
+    let title = titleElement.innerText;
+    console.log(title);
+    const nav = `<div class="navbar-wrapper">
+        <div class="navbar-content">
+            <div class="tt">
+                <img class="img-fluid" src="../pub/media/Banner_1_.png" />
+            </div>
+            <div>
+                <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
+                <div class="welcome-message">
+                    <span class="welcome-title">${title}</span>
+                </div>
+            </div>
+            <div>
+                <img class="img-fluid" src="../pub/media/Banner_3.png" />
+            </div>
+            <div>
+                <img class="img-fluid" src="../pub/media/Banner_4.png" />
+            </div>
+        </div>
+    </div>`;
+
+    let barnav = document.querySelector('nav[role="navigation"]');
+    barnav.innerHTML = nav;
+    // alert('working');
+});
