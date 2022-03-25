17865b501ee44088fedd32caff6071d1fc18fcbf
Dwight Carter <dwightcarter@gpinet.com>
2022-03-25 16:13:12 -0400

    class name changes

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ window.addEventListener('DOMContentLoaded', () => {
         <div class="welcome-message Mobile">
                     <span class="welcome-title">${title}</span>
                 </div>
-            <div class="tt">
-                <img class="img-fluid pp" src="../pub/media/Banner_1_.png" />
+            <div class="banner-first">
+                <img class="img-fluid banner-first" src="../pub/media/Banner_1_.png" />
             </div>
-            <div class="pp">
+            <div class="banner-second">
                 <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
                 <div class="welcome-message">
                     <span class="welcome-title">${title}</span>
                 </div>
             </div>
-            <div class="pp">
+            <div class="banner-second">
                 <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
             </div>
 
-            <div class="pp">
+            <div class="banner-second">
                 <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
             </div>
         </div>
@@ -28,30 +28,3 @@ window.addEventListener('DOMContentLoaded', () => {
     let barnav = document.querySelector('nav[role="navigation"]');
     barnav.innerHTML = nav;
 });
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     let titleElement = document.getElementsByTagName('title')[0];
-//     let title = titleElement.innerText;
-//     const nav = `<div class="navbar-wrapper">
-//         <div class="navbar-content">
-//             <div class="tt">
-//                 <img class="img-fluid" src="../pub/media/Banner_1_.png" />
-//             </div>
-//             <div>
-//                 <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
-//                 <div class="welcome-message">
-//                     <span class="welcome-title">${title}</span>
-//                 </div>
-//             </div>
-//             <div>
-//                 <img class="img-fluid" src="../pub/media/Banner_3.png" />
-//             </div>
-//             <div>
-//                 <img class="img-fluid" src="../pub/media/Banner_4.png" />
-//             </div>
-//         </div>
-//     </div>`;
-
-//     let barnav = document.querySelector('nav[role="navigation"]');
-//     barnav.innerHTML = nav;
-// });
