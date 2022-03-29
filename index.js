1a1416d6e64938f2f769645f3d3a440f81733334
Dwight Carter <dwightcarter@gpinet.com>
2022-03-29 10:01:24 -0400

    fixed responsoive bug

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ window.addEventListener('DOMContentLoaded', () => {
     const nav = `<div class="navbar-wrapper">
         <div class="navbar-content">
         <div class="banner-mobile">
+        <a href="index.html">
                 <img class="img-fluid banner-first-mobile" src="../pub/media/transportationlogo.png">
+                </a>
             </div>
         <div class="welcome-message Mobile">
                     <span class="welcome-title">${title}</span>
                 </div>
             <div class="banner-first">
+            <a href="/index.html">
                 <img class="img-fluid banner-first" src="../pub/media/Banner_1_.png" />
+                </a>
             </div>
             <div class="banner-second">
                 <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
