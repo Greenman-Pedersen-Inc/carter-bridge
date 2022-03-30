27085498ca24dabc5f83c25e1966c54b6aa385bb
Dwight Carter <dwightcarter@gpinet.com>
2022-03-30 15:48:59 -0400

    revised

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ window.addEventListener('DOMContentLoaded', () => {
     const nav = `<div class="navbar-wrapper">
         <div class="navbar-content">
         <div class="banner-mobile">
-        <a href="/index.html">
-                <img class="img-fluid banner-first-mobile" src="../pub/media/transportationlogo.png">
+        <a href="index.html">
+                <img class="img-fluid banner-first-mobile" src="transportationlogo.png">
                 </a>
             </div>
         <div class="welcome-message Mobile">
                     <span class="welcome-title">${title}</span>
                 </div>
             <div class="banner-first">
-            <a href="/index.html">
-                <img class="img-fluid banner-first" src="../pub/media/Banner_1_.png" />
+            <a href="index.html">
+                <img class="img-fluid banner-first" src="Banner_1_.png" />
                 </a>
             </div>
             <div class="banner-second">
-                <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
+                <img class="img-fluid color-block" src="Banner_2.png" />
                 <div class="welcome-message">
                     <span class="welcome-title">${title}</span>
                 </div>
             </div>
             <div class="banner-second">
-                <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
+                <img class="img-fluid color-block" src="Banner_2.png" />
             </div>
 
             <div class="banner-second">
-                <img class="img-fluid color-block" src="../pub/media/Banner_2.png" />
+                <img class="img-fluid color-block" src="Banner_2.png" />
             </div>
         </div>
     </div>`;
