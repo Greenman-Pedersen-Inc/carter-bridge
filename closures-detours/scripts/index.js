b1bec5091639f17cb612acabb2bbac0a946b8856
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2022-06-30 09:27:02 -0400

    updates to map style and some various formatting

--- closures-detours/scripts/index.js
+++ closures-detours/scripts/index.js
@@ -1,19 +1,19 @@
-﻿require(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/request', 'esri/widgets/Legend'], (
-    Map,
-    MapView,
-    FeatureLayer,
-    esriRequest,
-    Legend
-) => {
+﻿require([
+    'esri/Map',
+    'esri/views/MapView',
+    'esri/layers/FeatureLayer',
+    'esri/request',
+    'esri/widgets/Legend',
+], (Map, MapView, FeatureLayer, esriRequest, Legend) => {
     const currentPhase = 0;
     const phaseInformationLayer =
         'https://services1.arcgis.com/VLhaRwzp3uCQMr7y/ArcGIS/rest/services/mdx_2200023_00_Closures_20220426_PublicView/FeatureServer/0/query?where=1=1&f=json&outFields=*&returnGeometry=false';
     const map = new Map({
         // basemap: 'satellite',
-        // basemap: 'hybrid',
+        basemap: 'hybrid',
         // basemap: 'osm',
         // basemap: 'dark-gray-vector',
-        basemap: 'gray-vector',
+        // basemap: 'gray-vector',
         // basemap: 'streets-vector',
         // basemap: 'streets-night-vector',
         // basemap: 'streets-navigation-vector',
@@ -316,7 +316,8 @@
                 {
                     image: 'images/Phase-19-detour-4.jpg',
                     title: 'Detour 3',
-                    description: 'Turn right onto Washington St E. Continue straight until Leon Sullivan Way.',
+                    description:
+                        'Turn right onto Washington St E. Continue straight until Leon Sullivan Way.',
                 },
             ],
         },
@@ -343,7 +344,8 @@
                 {
                     image: 'images/Phase-21-detour-4.jpg',
                     title: 'Detour 3',
-                    description: 'Turn right onto Washington St E and continue straight until Leon Sullivan Way.',
+                    description:
+                        'Turn right onto Washington St E and continue straight until Leon Sullivan Way.',
                 },
             ],
         },
@@ -386,7 +388,8 @@
                 {
                     image: 'images/Phase-24-detour-2.jpg',
                     title: 'Detour 1',
-                    description: 'Continue on Christopher St and take a left on Capitol St. Continue to Lee St.',
+                    description:
+                        'Continue on Christopher St and take a left on Capitol St. Continue to Lee St.',
                 },
             ],
         },
@@ -571,7 +574,8 @@
                 {
                     image: 'images/Phase-38-detour-3.jpg',
                     title: 'Detour 4',
-                    description: 'Turn right onto Washington St E. Continue straight until Leon Sullivan Way.',
+                    description:
+                        'Turn right onto Washington St E. Continue straight until Leon Sullivan Way.',
                 },
             ],
         },
@@ -835,7 +839,8 @@
 
     function SelectorMenu(attachPoint) {
         this.domNode = document.createElement('div');
-        this.domNode.className = 'esri-component esri-layer-list esri-widget esri-widget--panel phase-selection';
+        this.domNode.className =
+            'esri-component esri-layer-list esri-widget esri-widget--panel phase-selection';
 
         const selectorDescription = document.createElement('p');
         selectorDescription.className = 'my-2 fw-bold';
