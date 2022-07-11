03e2d48606bcd05eb25cf9196b44ef12e8a9855f
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2022-07-11 14:31:24 -0400

    added the images to the photo map along with a position.

--- photo-map/scripts/index.js
+++ photo-map/scripts/index.js
@@ -1,9 +1,12 @@
-require(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/request'], (
-    Map,
-    MapView,
-    FeatureLayer,
-    esriRequest
-) => {
+require([
+    'esri/Map',
+    'esri/views/MapView',
+    'esri/layers/FeatureLayer',
+    'esri/request',
+    'esri/popup/content/ImageMediaInfo',
+    'esri/popup/content/support/ImageMediaInfoValue',
+    'esri/popup/content/MediaContent',
+], (Map, MapView, FeatureLayer, esriRequest, ImageMediaInfo, ImageMediaInfoValue, MediaContent) => {
     esriRequest(
         'https://services1.arcgis.com/VLhaRwzp3uCQMr7y/ArcGIS/rest/services/mdx_2200023_00_CameraLocations_Publicview/FeatureServer/0/query?where=1=1&f=json&outFields=*&returnGeometry=false'
     ).then(function (response) {
@@ -15,25 +18,54 @@ require(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/req
             map: map,
             center: [-81.63211, 38.35822], //Longitude, latitude
             zoom: 15,
+            popup: {
+                dockEnabled: true,
+                dockOptions: {
+                    buttonEnabled: false,
+                    breakpoint: false,
+                },
+            },
         });
 
         const photoLocations = [
-            { geometry: { type: 'point', longitude: -81.641, latitude: 38.356 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.637, latitude: 38.356 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.634, latitude: 38.357 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.636, latitude: 38.36 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.641, latitude: 38.361 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.643, latitude: 38.36 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.647, latitude: 38.36 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.624, latitude: 38.361 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.623, latitude: 38.361 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.621, latitude: 38.363 }, attributes: {} },
-            { geometry: { type: 'point', longitude: -81.631, latitude: 38.361 }, attributes: {} },
+            {
+                geometry: { type: 'point', longitude: -81.63531, latitude: 38.36019 },
+                attributes: { 'image-path': 'images/site-photos/under-bridge-1.jpg' },
+            },
+            {
+                geometry: { type: 'point', longitude: -81.63531, latitude: 38.36019 },
+                attributes: { 'image-path': 'images/site-photos/under-bridge-2.jpg' },
+            },
+            {
+                geometry: { type: 'point', longitude: -81.63531, latitude: 38.36019 },
+                attributes: { 'image-path': 'images/site-photos/under-bridge-3.jpg' },
+            },
+            {
+                geometry: { type: 'point', longitude: -81.63531, latitude: 38.36019 },
+                attributes: { 'image-path': 'images/site-photos/under-bridge-4.jpg' },
+            },
+            {
+                geometry: { type: 'point', longitude: -81.63531, latitude: 38.36019 },
+                attributes: { 'image-path': 'images/site-photos/under-bridge-5.jpg' },
+            },
         ];
 
         let layer = new FeatureLayer({
             source: photoLocations, // autocast as a Collection of new Graphic()
+            fields: [
+                {
+                    name: 'ObjectID',
+                    alias: 'ObjectID',
+                    type: 'oid',
+                },
+                {
+                    name: 'image-path',
+                    alias: 'image-path',
+                    type: 'string',
+                },
+            ],
             objectIdField: 'ObjectID',
+            outFields: ['*'],
             renderer: {
                 type: 'simple', // autocasts as new SimpleRenderer()
                 symbol: {
@@ -43,21 +75,26 @@ require(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/req
                     height: '64px',
                 },
             },
+            popupTemplate: {
+                title: 'Images for Closures',
+                content: [
+                    {
+                        type: 'media', // MediaContentElement
+                        mediaInfos: [
+                            {
+                                title: '<b>Parking Lot Closure Images</b>',
+                                type: 'image',
+                                caption: 'Current Parking Log Configuration',
+                                value: {
+                                    sourceURL: '{image-path}',
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
         });
 
         map.add(layer);
-
-        view.on('click', function (event) {
-            view.popup.autoOpenEnabled = false;
-
-            let lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
-            let lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
-
-            view.popup.open({
-                title: 'Reverse geocode: [' + lon + ', ' + lat + ']',
-                location: event.mapPoint,
-                content: 'This is a point of interest where a picture can go.',
-            });
         });
-    });
 });
